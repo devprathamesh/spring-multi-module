00f198b2210876a9e2860683a080e75d3836bc79
devprathamesh <bandivadekar.prathamesh@gmail.com>
2020-07-25 20:15:40 +0530

    Initial Commit

---
+++ api-module/src/main/java/dev/prathamesh/service/module/auther/AutherService.java
@@ -0,0 +1,16 @@
+package dev.prathamesh.service.module.auther;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Service;
+
+import dev.prathamesh.data.module.dto.Auther;
+
+@Service
+public class AutherService {
+
+	public Auther getAuther() {
+		return new Auther("Prathamesh", LocalDate.now());
+	}
+
+}
---
+++ api-module/src/test/java/dev/prathamesh/api/module/ApiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.prathamesh.api.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
